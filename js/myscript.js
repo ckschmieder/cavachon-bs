7e6b01191a831d3ef646a9c5a7bdeedfea5f2994
Chris Schmieder <ckschmieder@gmail.com>
2016-01-02 14:26:39 -0500

    Added Masonry.js and a temporary image gallery.

--- js/myscript.js
+++ js/myscript.js
@@ -53,4 +53,15 @@ $(function() {
   //Activate ReadMore plugin
   $('.puppy-rm').readmore({speed: 500});
 
+  //Initialize Masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });  
+
 });
